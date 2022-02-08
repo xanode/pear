5a998a234fa834e5e5c4c23d22d65b84ab96afcb
Benoit Marzelleau <benoit.marzelleau@telecom-sudparis.eu>
2022-02-08 16:56:15 +0100

    Create a body.

--- src/main/java/toxcore/dht/DHT.java
+++ src/main/java/toxcore/dht/DHT.java
@@ -1,4 +1,56 @@
 package toxcore.dht;
 
-public class DHT {
+import com.muquit.libsodiumjna.SodiumKeyPair;
+import com.muquit.libsodiumjna.SodiumLibrary;
+import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
+import com.sun.jna.Platform;
+
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class DHT implements Runnable {
+    private final String libraryPath;
+    final private byte[] publicKey;
+    final private byte[] privateKey;
+    final private int port;
+    private DatagramSocket socket;
+    private ConcurrentHashMap<byte[], InetAddress> nodes;
+    private byte[] buffer;
+    private ArrayList<Thread> servicingThread;
+    private boolean running = true;
+    public DHT(int port) throws SodiumLibraryException, SocketException {
+        // Load libsodium library first in order to generate public and private key
+        if (Platform.isWindows()) {
+            this.libraryPath = "C:/libsodium/libsodium.dll";
+        } else if (Platform.isMac()) {
+            this.libraryPath = "/usr/local/lib/libsodium.dylib";
+        } else {
+            this.libraryPath = "/usr/local/lib/libsodium.so";
+        }
+        SodiumLibrary.setLibraryPath(this.libraryPath);
+
+        // Then generate keys
+        SodiumKeyPair keyPair = SodiumLibrary.cryptoBoxKeyPair();
+        this.publicKey = keyPair.getPublicKey();
+        this.privateKey = keyPair.getPrivateKey(); // Should be loaded from file in real world, see Tox Doc
+
+        // Initialize network parameters
+        this.port = port;
+        this.socket = new DatagramSocket(this.port);
+        this.buffer = new byte[65536]; // Max length of UDP datagrams
+
+        // Initialize local hashed table
+        this.nodes = new ConcurrentHashMap<>();
+
+        // Initialize list that references started threads
+        this.servicingThread = new ArrayList<>();
+    }
+
+    public void run() {
+        // TODO: write function
+    }
 }
--- src/main/java/toxcore/dht/DHTRequestHandler.java
+++ src/main/java/toxcore/dht/DHTRequestHandler.java
@@ -1,4 +1,66 @@
 package toxcore.dht;
 
-public class DHTRequestHandler {
+import com.muquit.libsodiumjna.SodiumLibrary;
+import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class DHTRequestHandler implements Runnable {
+    final private byte[] publicKey;
+    final private byte[] privateKey;
+    private DatagramPacket packet;
+    private DatagramSocket socket;
+    private ConcurrentHashMap<byte[], InetAddress> nodes;
+    final private int port;
+    public DHTRequestHandler(DatagramSocket socket, DatagramPacket packet, int port, byte[] publicKey, byte[] privateKey, ConcurrentHashMap<byte[], InetAddress> nodes) {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+        this.socket = socket;
+        this.packet = packet;
+        this.nodes = nodes;
+        this.port = port;
+    }
+
+    public void run() {
+        // TODO: everything
+    }
+
+    private DatagramPacket DHTPacketBuilder(byte[] type, byte[] receiverPublicKey, byte[] payload, InetAddress receiverInetAddress) throws SodiumLibraryException {
+        // Generate nonce
+        byte[] nonce = SodiumLibrary.randomBytes(SodiumLibrary.cryptoBoxNonceBytes().intValue());
+        // Encrypt payload
+        byte[] encryptedPayload = SodiumLibrary.cryptoBoxEasy(payload, nonce, receiverPublicKey, this.privateKey);
+
+        // Collect data
+        byte[] data = new byte[type.length+this.publicKey.length+nonce.length+encryptedPayload.length];
+        // TODO: build packet
+
+        // Effective construction of datagram packet
+        return new DatagramPacket(data, data.length, receiverInetAddress, this.port);
+    }
+
+    private Integer getDistance(byte[] nodePublicKey) {
+        // Return distance between two nodes by calculating Public_Key_1 XOR Public_Key_2 (public keys in big endian format)
+        // TODO: compute distances
+        return 0;
+    }
+
+    private ArrayList<byte[]> getClosestNodes(byte[] nodePublicKey) {
+        // Get closest nodes in our own private list
+        ArrayList<byte[]> closestNodes = new ArrayList<>();
+        // TODO: compute distances, sort and return them
+        return closestNodes;
+    }
+
+    private InetAddress lookupNode(byte[] nodePublicKey) {
+        // Recursive algorithm that lookup a node in the full dht
+        // TODO: write the algorithm
+        return InetAddress.getLoopbackAddress();
+    }
+
+    // TODO: Add missing functions!
 }
