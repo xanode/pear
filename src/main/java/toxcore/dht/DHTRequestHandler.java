c5bacfff64d39cd5e20b40cb3a6315434fa057dc
Benoit Marzelleau <benoit.marzelleau@telecom-sudparis.eu>
2022-02-16 13:27:32 +0100

    Basic completion of run method of DHTRequestHandler.

--- src/main/java/toxcore/dht/DHTRequestHandler.java
+++ src/main/java/toxcore/dht/DHTRequestHandler.java
@@ -9,6 +9,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DHTRequestHandler implements Runnable {
@@ -21,7 +22,23 @@ public class DHTRequestHandler implements Runnable {
 
     @Override
     public void run() {
-        // TODO: everything
+        // Parse data
+        byte[] packetData = this.packet.getData();
+        byte[] packetType = Arrays.copyOfRange(packetData, 0, 1);
+        byte[] senderPublicKey = Arrays.copyOfRange(packetData, 1, 33);
+        byte[] nonce = Arrays.copyOfRange(packetData, 33, 57);
+        byte[] encryptedPayload = Arrays.copyOfRange(packetData, 58, this.packet.getLength());
+        // Select action
+        switch (packetType[0]) {
+            case (byte) 0:
+                // Ping request
+                break;
+            case (byte) 1:
+                // Ping response
+                break;
+            default:
+                // Invalid Type
+        }
     }
 
     private DatagramPacket DHTPacketBuilder(byte[] type, byte[] receiverPublicKey, byte[] payload, InetAddress receiverInetAddress) throws SodiumLibraryException {
