8ba5b15f6abcf6b8e89f41d6e58fdf7cac0844a5
Benoit Marzelleau <benoit.marzelleau@telecom-sudparis.eu>
2022-04-04 00:16:00 +0200

    Update unit test for DHT class.

--- src/test/java/toxcore/dht/DHTTest.java
+++ src/test/java/toxcore/dht/DHTTest.java
@@ -2,9 +2,7 @@ package toxcore.dht;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.net.SocketException;
-import java.util.Random;
-
+import com.muquit.libsodiumjna.SodiumLibrary;
 import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,31 +12,36 @@ import org.junit.jupiter.api.Test;
 class DHTTest {
 
     @Test
-    @DisplayName("Instanciating a DHT and closing it")
-    void testDHT() throws SocketException, SodiumLibraryException {
-        Random r = new Random();
-        int port = r.nextInt(65535 - 1024) + 1024;
-        DHT dht = new DHT(port);
-        dht.close();
+    @DisplayName("Instanciating a DHT")
+    void testDHT() throws SodiumLibraryException {
+        DHT dht = new DHT();
     }
 
     @Test
-    @DisplayName("Return the correct port")
-    void testGetPort() throws SocketException, SodiumLibraryException {
-        Random r = new Random();
-        int port = r.nextInt(65535 - 1024) + 1024;
-        DHT dht = new DHT(port);
-        assertEquals(port, dht.getPort());
+    @DisplayName("Test that the public key is the right size.") // Stupid test, but it's a start.
+    void testPublicKeySize() throws SodiumLibraryException {
+        DHT dht = new DHT();
+        assertEquals(dht.getPublicKey().length, DHT.CRYPTO_PUBLIC_KEY_SIZE);
     }
 
     @Test
-    @DisplayName("Get keys")
-    void testGetKeys() throws SocketException, SodiumLibraryException {
-        Random r = new Random();
-        int port = r.nextInt(65535 - 1024) + 1024;
-        DHT dht = new DHT(port);
-        assertEquals(32, dht.getPublicKey().length);
-        assertEquals(32, dht.getPrivateKey().length);
+    @DisplayName("Test that a generated nonce is the right size.") // Stupid test, but it's a start.
+    void testNonceSize() throws SodiumLibraryException {
+        DHT dht = new DHT();
+        assertEquals((byte) SodiumLibrary.cryptoBoxNonceBytes().intValue(), DHT.CRYPTO_NONCE_SIZE); // This is necessary for encryption
+        assertEquals(dht.generateNonce().length, DHT.CRYPTO_NONCE_SIZE);
+    }
+
+    @Test
+    @DisplayName("Test that the encryption is working")
+    void testEncryption() throws SodiumLibraryException {
+        DHT sender = new DHT();
+        DHT receiver = new DHT();
+        byte[] nonce = sender.generateNonce();
+        byte[] plaintext = "Hello World".getBytes();
+        byte[] ciphertext = sender.encrypt(receiver.getPublicKey(), nonce, plaintext);
+        byte[] decrypted = receiver.decrypt(sender.getPublicKey(), nonce, ciphertext);
+        assertEquals(new String(plaintext), new String(decrypted));
     }
 
 }
