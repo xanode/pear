2a756d5dcc152b8b3e84bc9e9fa091a7d50c2403
Siffrein Sigy <hello@siffreinsigy.me>
2022-04-20 15:15:06 +0200

    Test instanciating a client list with an ipv6 node

--- src/test/java/toxcore/dht/ClientListTest.java
+++ src/test/java/toxcore/dht/ClientListTest.java
@@ -2,42 +2,48 @@ package toxcore.dht;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.lang.RuntimeException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Random;
 
 import com.muquit.libsodiumjna.SodiumLibrary;
 import com.muquit.libsodiumjna.exceptions.SodiumLibraryException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import com.sun.jna.Platform;
 
 
 @DisplayName("Test ClientList class")
 public class ClientListTest {
+    Random rd = new Random();
 
     @Test
-    @DisplayName("Instanciate a ClientList")
-    void testClientList() {
-        new ClientList(32, getRandomNode());
+    @DisplayName("Instanciate a ClientList with IPv4 node")
+    void testClientListIPv4() throws SodiumLibraryException, UnknownHostException {
+        new ClientList(32, generateIPv4Node());
+    }
+
+    @Test
+    @DisplayName("Instanciate a ClientList with IPv6 node")
+    void testClientListIPv6() throws SodiumLibraryException, UnknownHostException {
+        new ClientList(32, generateIPv6Node());
     }
 
     @Test
     @DisplayName("Add a node in the client list")
-    void testAdd() {
-        ClientList clientList = new ClientList(32, getRandomNode());
+    void testAdd() throws SodiumLibraryException, UnknownHostException {
+        ClientList clientList = new ClientList(32, generateIPv4Node());
 
-        Node node = getRandomNode();
+        Node node = generateIPv4Node();
 
         assertEquals(clientList.add(node), true);
     }
 
     @Test
-    @DisplayName("Remove a public key in the client list")
-    void testRemove() {
-        ClientList clientList = new ClientList(32, getRandomNode());
-        Node node = getRandomNode();
+    @DisplayName("Remove a node from a client list")
+    void testRemove() throws SodiumLibraryException, UnknownHostException {
+        ClientList clientList = new ClientList(32, generateIPv4Node());
+        Node node = generateIPv4Node();
         clientList.add(node);
 
         // Remove the key
@@ -48,56 +54,51 @@ public class ClientListTest {
     }
 
     @Test
-    @DisplayName("Add a client in a full list")
-    void testAddFullList() {
-        ClientList clientList = new ClientList(32, getRandomNode());
+    @DisplayName("Add a node in a full list")
+    void testAddFullList() throws SodiumLibraryException, UnknownHostException {
+        ClientList clientList = new ClientList(32, generateIPv4Node());
 
         // Fill the list
         for (int i = 0; i < 32; i++) {
-            if (clientList.add(getRandomNode()) == true) {
-            }
+            clientList.add(generateIPv4Node());
         }
 
         // Try to add a new key
-        assertEquals(clientList.add(getRandomNode()), false);
+        assertEquals(clientList.add(generateIPv4Node()), false);
     }
 
-    /**
-     * Get a random key.
-     *
-     * @return A random key.
-     */
-    static Node getRandomNode() {
-        // Load libsodium library if necessary
-        try {
-            SodiumLibrary.libsodiumVersionString(); // Does nothing if libsodium is already loaded
-        } catch (RuntimeException ignored) {
-            String libraryPath;
-            if (Platform.isWindows()) {
-                libraryPath = "C:/libsodium/libsodium.dll";
-            } else if (Platform.isMac()) {
-                libraryPath = "/usr/local/lib/libsodium.dylib";
-            } else {
-                libraryPath = "/usr/lib64/libsodium.so.23";
-            }
-            SodiumLibrary.setLibraryPath(libraryPath);
-        }
-        // Generate a random node
-        byte[] randomInetAddress = new byte[4];
-        Random rd = new Random();
+    // ============================================================
+    // HELPERS
+    // ============================================================
+    Node generateIPv4Node() throws SodiumLibraryException, UnknownHostException {
+        byte[] randomInetAddress = new byte[Network.SIZE_IP4];
+        Node node = null;
+
         rd.nextBytes(randomInetAddress);
-        randomInetAddress[0] = (byte) (rd.nextInt(224) + 1); // Force the address not to be a multicast address (RFC 5771)
-        try {
-            Node node = new Node(
-                    null, // DHT is not used in those tests
-                    SodiumLibrary.cryptoBoxKeyPair().getPublicKey(),
-                    InetAddress.getByAddress(randomInetAddress),
-                    rd.nextInt(65536)
-            );
-            return node;
-        } catch (SodiumLibraryException | UnknownHostException ignored) {
-            // Can't happen
-        }
-        return null;
+        randomInetAddress[0] = (byte) (rd.nextInt(224) + 1); // Force the address not to be a multicast address
+
+        node = new Node(
+                new DHT(), // Necessary to initialize the Sodium library
+                SodiumLibrary.cryptoBoxKeyPair().getPublicKey(),
+                InetAddress.getByAddress(randomInetAddress),
+                rd.nextInt(65536));
+
+        return node;
+    }
+
+    Node generateIPv6Node() throws SodiumLibraryException, UnknownHostException {
+        byte[] randomInetAddress = new byte[Network.SIZE_IP6];
+        Node node = null;
+
+        rd.nextBytes(randomInetAddress);
+        randomInetAddress[0] = (byte) (rd.nextInt(224) + 1); // Force the address not to be a multicast address
+
+        node = new Node(
+                new DHT(), // Necessary to initialize the Sodium library
+                SodiumLibrary.cryptoBoxKeyPair().getPublicKey(),
+                InetAddress.getByAddress(randomInetAddress),
+                rd.nextInt(65536));
+
+        return node;
     }
 }
