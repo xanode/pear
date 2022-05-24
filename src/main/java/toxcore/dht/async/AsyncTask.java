bb715c497ae4ba5244e6c271acca255b4b163a18
xanode <benoit.marzelleau@telecom-sudparis.eu>
2022-05-24 10:40:56 +0200

    Add half sync half async classes.

---
+++ src/main/java/toxcore/dht/async/AsyncTask.java
@@ -0,0 +1,32 @@
+package toxcore.dht.async;
+
+import java.util.concurrent.Callable;
+
+public interface AsyncTask<O> extends Callable<O> {
+    /**
+     * Is called in context of caller thread before call to {@link #call()}. Small tasks can be
+     * performed here so that the performance penalty of context switching is not incurred in case
+     * of invalid requests.
+     */
+    void onPreCall();
+
+    /**
+     * A callback called after the result is successfully computed by {@link #call()}. This
+     * method is called in context of background thread.
+     */
+    void onPostCall(O result);
+
+    /**
+     * A callback called if computing the task resulted in some exception. This method is called
+     * when either of {@link #call()} or {@link #onPreCall()} throw any exception.
+     *
+     * @param throwable error cause
+     */
+    void onError(Throwable throwable);
+
+    /**
+     * This method is called in context of background thread. The computation of task reside here.
+     */
+    @Override
+    O call() throws Exception;
+}
---
+++ src/main/java/toxcore/dht/async/AsynchronousService.java
@@ -0,0 +1,66 @@
+package toxcore.dht.async;
+
+import java.util.concurrent.*;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class AsynchronousService {
+    /**
+     * This represents the queuing layer as well as synchronous layer of the patterns. The thread pool
+     * contains worker threads which execute the tasks in blocking/synchronous manner. Long running
+     * tasks are performed in the background which does not affect the performance of main thread.
+     */
+    private final ExecutorService service;
+
+    /**
+     * Creates an asynchronous service using {@code workQueue} as communication channel between
+     * asynchronous layer and synchronous layer.
+     */
+    public AsynchronousService(BlockingQueue<Runnable> workQueue) {
+        this.service = new ThreadPoolExecutor(10, 10, 10, TimeUnit.SECONDS, workQueue);
+    }
+
+    /**
+     * A non-blocking method which performs the task provided in the background and returns immediately.
+     * On successful completion of the task the result is posted back using callback method {@link
+     * AsyncTask#onPostCall(Object)}, if task execution is unable to complete normally due to some
+     * exception then the reason for error is posted back using callback method {@link AsyncTask#onError(Throwable)}.
+     */
+    public <T> void execute(final AsyncTask<T> task) {
+        try {
+            // Execute small tasks
+            task.onPreCall();
+        } catch (Exception e) {
+            task.onError(e);
+            return;
+        }
+
+        service.submit(new FutureTask<T>(task) {
+            @Override
+            protected void done() {
+                super.done();
+                try {
+                    // Called in context of background thread
+                    task.onPostCall(this.get());
+                } catch (InterruptedException ignored) {
+                    // Shouldn't occur
+                } catch (ExecutionException e) {
+                    task.onError(e.getCause());
+                }
+            }
+        });
+    }
+
+    /**
+     * Stops the pool of workers. This is blocking call to wait for all tasks to be completed.
+     */
+    public void close() {
+        service.shutdown();
+        try {
+            service.awaitTermination(10, TimeUnit.SECONDS);
+        } catch(InterruptedException e) {
+            log.error("Error waiting for executor service shutdown!");
+        }
+    }
+
+}
