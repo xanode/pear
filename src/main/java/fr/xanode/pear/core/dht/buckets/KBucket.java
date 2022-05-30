b16aedffe985228987c18b3c07e5cfd20eaff925
xanode <benoit.marzelleau@telecom-sudparis.eu>
2022-05-30 18:55:05 +0200

    Change attribute name and a remove method.

--- src/main/java/fr/xanode/pear/core/dht/buckets/KBucket.java
+++ src/main/java/fr/xanode/pear/core/dht/buckets/KBucket.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 public class KBucket {
 
     @NonNull private final int k; // Original paper advise 20
-    private final ArrayList<Node> bucket = new ArrayList<>();
+    private final ArrayList<Node> nodes = new ArrayList<>();
 
     /**
      * Update this KBucket with a new node.
@@ -22,27 +22,27 @@ public class KBucket {
     public void update(Node node) {
         log.info("Updating bucket with new node...");
         // If the node already exist in the bucket, move it to the tail of the list
-        if (this.bucket.contains(node)) {
+        if (this.nodes.contains(node)) {
             log.info("Node already exists in the bucket.");
             log.info("Moving node to the tail of the bucket...");
-            this.bucket.remove(node);
-            this.bucket.add(node);
+            this.nodes.remove(node);
+            this.nodes.add(node);
             log.info("Node moved to the tail of the list.");
         } else {
             // If not in the bucket and the bucket isn't full, insert it at the tail
-            if (this.bucket.size() < this.k) {
+            if (this.nodes.size() < this.k) {
                 log.info("The node isn't in the bucket and the bucket isn't full.");
                 log.info("Adding node...");
-                this.bucket.add(node);
+                this.nodes.add(node);
                 log.info("Node added.");
             } else {
                 // If the bucket is full
                 log.info("The node isn't in the bucket but the bucket is full.");
                 log.info("Testing if the node is alive...");
-                if (!this.bucket.get(this.k-1).isAlive()) {
+                if (!this.nodes.get(this.k-1).isAlive()) {
                     // Replace last node if it isn't alive anymore
                     log.info("The node seems alive, adding it.");
-                    this.bucket.set(this.k-1, node);
+                    this.nodes.set(this.k-1, node);
                     log.info("Node added.");
                 }
             }
@@ -51,11 +51,19 @@ public class KBucket {
     }
 
     /**
+     * Remove a node from the bucket.
+     * @param node the node to remove
+     */
+    public void remove(Node node) {
+        this.nodes.remove(node);
+    }
+
+    /**
      * Get the actual size of the KBucket.
      * @return The size of the KBucket.
      */
     public int getSize() {
-        return this.bucket.size();
+        return this.nodes.size();
     }
 
     /**
@@ -64,7 +72,7 @@ public class KBucket {
      * @return True if the node is in the KBucket, false otherwise.
      */
     public Node contains(Node node) {
-        for (Node n : this.bucket) {
+        for (Node n : this.nodes) {
             if (n.equals(node)) {
                 return n;
             }
